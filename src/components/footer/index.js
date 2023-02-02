c9765723cb2239042dd6f8d3f5861d7155d07672
Jhony Freitas <jhony00._@hotmail.com>
2023-02-01 21:00:28 -0300

    footer style

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,3 +1,13 @@
+import styled from "styled-components"
+
 export const Footer = () =>{
-    return <p> MKS sistemas #copy# Todos os direitos reservados </p>
+    return <P> MKS sistemas © Todos os direitos reservados </P>
 }
+
+const P = styled.p`
+    display: block;
+    background-color: #eeeeee;
+    width: 1440px;
+    padding: 4px;
+    text-align: center;
+`
